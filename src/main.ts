95a63a593b8b6a51684fca871deb3d81f96cb4c1
Valdo <vteixeiraromao@gmail.com>
2025-01-02 19:17:37 +0000

    Add univers map (not colored yet)

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,33 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
 console.log('Script started successfully');
-
-let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+  console.log('Scripting API ready');
+  console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+  WA.room.onEnterLayer("auditorium-zone").subscribe(() => {
+    WA.room.hideLayer("auditorium-roof");
+    WA.room.hideLayer("auditorium-walls");
+    WA.room.hideLayer("auditorium-sign");
+  });
+  WA.room.onLeaveLayer("auditorium-zone").subscribe(() => {
+    WA.room.showLayer("auditorium-roof");
+    WA.room.showLayer("auditorium-walls");
+    WA.room.showLayer("auditorium-sign");
+  });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
+  WA.room.area.onEnter("auditorium-focus").subscribe(() => {
+    WA.room.hideLayer("facade-furniture-bg");
+    WA.room.hideLayer("facade-furniture-fg");
+    WA.room.hideLayer("facade");
+  });
+  WA.room.area.onLeave("auditorium-focus").subscribe(() => {
+    WA.room.showLayer("facade-furniture-bg");
+    WA.room.showLayer("facade-furniture-fg");
+    WA.room.showLayer("facade");
+  });
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
