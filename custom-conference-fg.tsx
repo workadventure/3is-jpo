2e25ecf5f345ebe80aeafe4e0cd19e9b010aee9f
zijerk <cheroy.killian@gmail.com>
2025-05-06 17:18:49 +0200

    Projet Etudiant/Cursus

---
+++ custom-conference-fg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="custom-conference-fg" tilewidth="32" tileheight="32" tilecount="18500" columns="185">
+ <image source="tilesets/custom-conference-fg.png" width="5920" height="3200"/>
+</tileset>
