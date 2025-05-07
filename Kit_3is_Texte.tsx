0e918ef23aaa3164dce8e28539d2083b366540d2
zijerk <cheroy.killian@gmail.com>
2025-05-07 15:50:51 +0200

    Changement Mercredi 07-05

--- Kit_3is_Texte.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Kit_3is_Texte" tilewidth="32" tileheight="32" tilecount="414" columns="23">
- <image source="tilesets/Kit_3is_Texte.png" width="760" height="578"/>
-</tileset>
